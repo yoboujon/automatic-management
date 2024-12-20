691c55f2b893e6ff5681e269352aa53241fa3e4f
Lemonochrme <robin.marn.pro@gmail.com>
2024-12-20 11:08:26 +0100

    Add Eureka client support to actuator, decision, notification, room, and sensor services

--- app/actuator-service/src/main/java/insa/application/app/rest/ActuatorServiceApplication.java
+++ app/actuator-service/src/main/java/insa/application/app/rest/ActuatorServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 public class ActuatorServiceApplication {
 
     public static void main(String[] args) {
