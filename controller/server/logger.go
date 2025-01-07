6d068b295694090fa53d17e0462fcecfc3fb7cd1
Yohan Boujon <yo.boujon@gmail.com>
2025-01-07 12:04:22 +0100

    Controller: Added time in logger.

--- controller/server/logger.go
+++ controller/server/logger.go
@@ -1,34 +1,48 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type colorFormat string
 type LogLevel int
 
 const (
-	BLUE   colorFormat = "\033[34m"
-	GREEN  colorFormat = "\033[32m"
-	YELLOW colorFormat = "\033[33m"
-	RED    colorFormat = "\033[31m"
-	RESET  colorFormat = "\033[0m"
+	BLUE      colorFormat = "\033[34m"
+	GREEN     colorFormat = "\033[32m"
+	YELLOW    colorFormat = "\033[33m"
+	RED       colorFormat = "\033[31m"
+	LIGHT_RED colorFormat = "\033[91m"
+	WHITE     colorFormat = "\033[97m"
+	RESET     colorFormat = "\033[0m"
 )
 
 const (
-	INFO LogLevel = iota
+	NOLOG LogLevel = iota
+	INFO
 	CORRECT
 	WARNING
 	ERROR
 )
 
 var colorPrint = map[LogLevel]colorFormat{
-	INFO:    BLUE,
+	NOLOG:   "",
+	INFO:    BLUE + "[INFO]" + RESET,
 	CORRECT: GREEN,
-	WARNING: YELLOW,
-	ERROR:   RED,
+	WARNING: YELLOW + "[WARN]",
+	ERROR:   RED + "[ERR.]" + LIGHT_RED,
 }
 
 func Logformat(level LogLevel, format string, args ...interface{}) {
-	color := colorPrint[level] // Get the color for the given log level
+	header := colorPrint[level]
 	message := fmt.Sprintf(format, args...)
-	fmt.Printf("%s%s%s", color, message, RESET) // Print with color and reset
+	currentTime := time.Now()
+	time := fmt.Sprintf("[%02d:%02d:%02d]", currentTime.Hour(), currentTime.Minute(), currentTime.Second())
+	if level != INFO {
+		fmt.Printf("%s%s\t%s%s", header, time, message, RESET)
+	} else {
+		fmt.Printf("%s%s\t%s", header, time, message)
+	}
+
 }
