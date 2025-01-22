83c664cac2540e7817eb8281b9fffa6716208319
Yohan Boujon <yo.boujon@gmail.com>
2025-01-22 00:06:28 +0000

    App: Gathering temperature and ppm from Decision. Making decisions in a loop.

--- app/decision-service/src/main/java/insa/application/helpapp/rest/DecisionApplication.java
+++ app/decision-service/src/main/java/insa/application/helpapp/rest/DecisionApplication.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import insa.application.helpapp.rest.DecisionList.ActionEnum;
 import insa.application.helpapp.rest.DecisionList.SensorEnum;
@@ -14,7 +18,8 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
-public class DecisionApplication implements CommandLineRunner {
+@EnableScheduling
+public class DecisionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DecisionApplication.class, args);
@@ -29,10 +34,50 @@ public class DecisionApplication implements CommandLineRunner {
         return decisionRepository.findAll();
     }
 
+    // Bean to do Rest requests
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    private float getRequest(String url) {
+        RestTemplate restTemplate = new RestTemplate();
+        SensorResponse response = restTemplate.getForObject(url, SensorResponse.class);
+        return response != null ? response.getValue() : 0;
+    }
+    
+
     // Main loop for the decision application
-    @Override
-    public void run(String... args) {
-        DecisionList d = new DecisionList(ActionEnum.HEATING_START, SensorEnum.TEMPERATURE);
-        decisionRepository.save(new Decision(d));
+    @Scheduled(fixedRate = 200)
+    public void decisionLoop() {
+        float temperature = getRequest("http://localhost:8085/sensors/1");
+        float ppm = getRequest("http://localhost:8085/sensors/0");
+
+        if(temperature < 20) {
+            // startHeating()
+            DecisionList d = new DecisionList(ActionEnum.HEATING_START, SensorEnum.TEMPERATURE);
+            decisionRepository.save(new Decision(d));
+            // closeWindow()
+            d = new DecisionList(ActionEnum.HEATING_START, SensorEnum.TEMPERATURE);
+            decisionRepository.save(new Decision(d));
+        }
+
+        if(temperature > 22) {
+            // stopHeating()
+            DecisionList d = new DecisionList(ActionEnum.HEATING_STOP, SensorEnum.TEMPERATURE);
+            decisionRepository.save(new Decision(d));
+        }
+
+        if(temperature > 25) {
+            // openWindow()
+            DecisionList d = new DecisionList(ActionEnum.WINDOWS_OPEN, SensorEnum.TEMPERATURE);
+            decisionRepository.save(new Decision(d));
+        }
+
+        if(ppm >= 800) {
+            // openWindow()
+            DecisionList d = new DecisionList(ActionEnum.WINDOWS_OPEN, SensorEnum.CARBON_DIOXIDE);
+            decisionRepository.save(new Decision(d));
+        }
     }
 }
---
+++ app/decision-service/src/main/java/insa/application/helpapp/rest/SensorResponse.java
@@ -0,0 +1,9 @@
+package insa.application.helpapp.rest;
+
+public class SensorResponse {
+    private float value;
+
+    public float getValue() {
+        return value;
+    }
+}
