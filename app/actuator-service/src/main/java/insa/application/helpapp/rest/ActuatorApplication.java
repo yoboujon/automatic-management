68abe6b7aad00d36640565cb6818f7ad71dd9e14
Lemonochrme <robin.marn.pro@gmail.com>
2025-01-24 21:18:25 +0100

    Simplified because actuator didnt worked

--- app/actuator-service/src/main/java/insa/application/helpapp/rest/ActuatorApplication.java
+++ app/actuator-service/src/main/java/insa/application/helpapp/rest/ActuatorApplication.java
@@ -1,32 +1,47 @@
 package insa.application.helpapp.rest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
+import java.util.Arrays;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/actuators")
 public class ActuatorApplication {
-
-    @Autowired
-    private ActuatorService actuatorService;
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+
+        // Fetch actuators from external API and print to console
+        fetchActuators();
     }
 
-    @GetMapping
-    public List<Actuator> getActuators() {
-        return actuatorService.getAllActuators();
-    }
+    public static void fetchActuators() {
+        // URL of the external API
+        String apiUrl = "http://localhost:8085/actuators/";
 
-    @PutMapping("/{id}")
-    public Actuator updateActuatorState(@PathVariable Long id, @RequestBody ActuatorStateRequest stateRequest) {
-        return actuatorService.updateActuatorState(id, stateRequest.getState());
+        // RestTemplate to make HTTP requests
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            // Fetch actuators as an array
+            Actuator[] actuators = restTemplate.getForObject(apiUrl, Actuator[].class);
+
+            if (actuators != null) {
+                // Print each actuator to the console
+                Arrays.stream(actuators).forEach(actuator -> {
+                    System.out.println("Name: " + actuator.getName());
+                    System.out.println("Type: " + actuator.getType());
+                    System.out.println("ID: " + actuator.getId());
+                    System.out.println("Value: " + actuator.getValue());
+                    System.out.println("Room: " + actuator.getRoom());
+                    System.out.println("--------------------------------");
+                });
+            } else {
+                System.out.println("No actuators found.");
+            }
+        } catch (Exception e) {
+            System.err.println("Error fetching actuators: " + e.getMessage());
+        }
     }
 }
--- app/actuator-service/src/main/java/insa/application/helpapp/rest/ActuatorRepository.java
+++
@@ -1,6 +0,0 @@
-package insa.application.helpapp.rest;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ActuatorRepository extends JpaRepository<Actuator, Long> {
-}
