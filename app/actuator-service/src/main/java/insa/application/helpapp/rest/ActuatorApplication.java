f7b34b4a068c4eaed66bb04296d60171b3f8877b
Lemonochrme <robin.marn.pro@gmail.com>
2025-01-24 20:15:25 +0100

    Actuator Boilerplate

--- app/actuator-service/src/main/java/insa/application/helpapp/rest/ActuatorApplication.java
+++ app/actuator-service/src/main/java/insa/application/helpapp/rest/ActuatorApplication.java
@@ -2,35 +2,21 @@ package insa.application.helpapp.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/actuators")
+@RequestMapping("/actuator")
 public class ActuatorApplication {
-
-    private final ActuatorService actuatorService;
-
-    public ActuatorApplication(ActuatorService actuatorService) {
-        this.actuatorService = actuatorService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
-    @GetMapping
-    public List<Actuator> getAllActuators() {
-        return actuatorService.getAllActuators();
-    }
-
-    @PutMapping("/{room}/{type}")
-    public Actuator updateActuatorState(
-            @PathVariable int room,
-            @PathVariable String type,
-            @RequestBody ActuatorRequest actuatorRequest) {
-        return actuatorService.updateActuatorState(room, type, actuatorRequest.getState());
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
     }
 }
